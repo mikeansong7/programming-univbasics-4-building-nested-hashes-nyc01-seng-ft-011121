100adf76737d32cd7a8593ddb97c1ba782381993
Michael B Ansong <michael.ansong7@gmail.com>
2020-12-30 20:35:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,18 @@
+def base_hash
+  monopoly =  {:railroads=>{}}
+end
+
+def monopoly_with_second_tier
+  monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>{},
+    :names=>{}
+   }
+  }
+end
+  
 monopoly =  {
   :railroads=>
   {
